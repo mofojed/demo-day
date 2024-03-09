4fb64e550c799eb8d969c08e05d8d657965cdef2
mikebender <mikebender@deephaven.io>
2024-03-09 14:35:20 -0500

    Draw user video on top of screenshare

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ const log = Log.module("App");
 
 function App() {
   const videoElement = useRef<HTMLVideoElement>(null);
+  const screenElement = useRef<HTMLVideoElement>(null);
   const canvasElement = useRef<HTMLCanvasElement>(null);
   const width = 1920;
   const height = 1080;
@@ -17,25 +18,29 @@ function App() {
 
   const handleStart = useCallback(async () => {
     try {
-      // const videoStream = await navigator.mediaDevices.getUserMedia({
-      //   video: true,
-      // });
-      const videoStream = await navigator.mediaDevices.getDisplayMedia({
+      const videoStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+      const screenStream = await navigator.mediaDevices.getDisplayMedia({
         video: {
           displaySurface: "browser",
         },
       });
-      log.info("Got video stream", videoStream);
+      log.info("Got video streams", screenStream, videoStream);
+
       // videoElement.current!.srcObject = videoStream;
+
       const video = videoElement.current!;
+      const screen = screenElement.current!;
       const canvas = canvasElement.current!;
       const context = canvas.getContext("2d")!;
       video.srcObject = videoStream;
+      screen.srcObject = screenStream;
 
       setInterval(function drawFrame() {
         // This is just an example, it's showing how to render it and then convert it to greyscale
         // However this is re-rendering in the same canvas. We may need to do it in another canvas so it can be layered properly...
-        context.drawImage(video, 0, 0, width, height);
+        context.drawImage(screen, 0, 0, width, height);
         const frame = context.getImageData(0, 0, width, height);
         const l = frame.data.length / 4;
 
@@ -51,6 +56,8 @@ function App() {
           frame.data[i * 4 + 2] = grey;
         }
         context.putImageData(frame, 0, 0);
+
+        context.drawImage(video, 100, 100, 320, 240);
       }, 16);
     } catch (e) {
       log.error("Unable to get video stream", e);
@@ -68,6 +75,14 @@ function App() {
           autoPlay
           style={{ display: "none" }}
           ref={videoElement}
+        ></video>
+        <video
+          id="video"
+          width={width}
+          height={height}
+          autoPlay
+          style={{ display: "none" }}
+          ref={screenElement}
         ></video>
         <button onClick={handleStart}>Start</button>
         <canvas
